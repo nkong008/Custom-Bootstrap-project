0204ae63f44cdc9ce8104e4386811bf84e8197f0
nkong008 <nkong2020@gmail.com>
2023-11-02 14:18:45 -0500

    fixed javascriot issues by ading type is module attribute to script tag

---
+++ js/index.js
@@ -0,0 +1,4 @@
+// const bootstrap = require('bootstrap');
+import "../node_modules/bootstrap/js/bootstrap.modules";
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+console.log("test");
