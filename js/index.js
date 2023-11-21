24bed81208f1b050a321c30b5d99e4780dcbb4d5
nkong008 <nkong2020@gmail.com>
2023-11-21 15:08:52 -0600

    created a carousel using bootstrap

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@
 
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
+const myCarouselElement = document.querySelector('#myCarousel')
 
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false,
+  pause: "hover"
+})
 
 console.log("test");
